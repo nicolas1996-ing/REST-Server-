ce94f0e67580beb85632e12a4c3cd9752299969e
J. Nicolas Aristizabal R <josenicolasaristizabalramirez@gmail.com>
2022-10-15 10:12:35 -0500

    update router

--- models/server.js
+++ models/server.js
@@ -45,8 +45,8 @@ class Server {
   }
 
   listen() {
-    this.app.listen(this.port, () => {
-      console.log(`App listening on port ${this.port}`);
+    this.app.listen(process.env.PORT || 8080, () => {
+      console.log(`App listening on port ${process.env.PORT || 8080}`);
     });
   }
 }
